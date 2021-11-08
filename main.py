fe2887db597e74d5e6564334712c464fd2d4b5ab
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2021-11-08 18:46:59 +0530

    Reset the ball to center when padlle misses it

--- main.py
+++ main.py
@@ -35,4 +35,11 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
+    # detect right paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+
 screen.exitonclick()
