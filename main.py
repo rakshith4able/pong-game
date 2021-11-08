b8689fd6987f3e253c72e2a4726cd65936337069
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2021-11-08 13:57:21 +0530

    created Paddle class

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 
-screen.setup(width=800,height=600)
+screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
+screen.tracer(0)
+l_paddle = Paddle((-350,0))
+r_paddle =  Paddle((350,0))
+screen.update()
+screen.listen()
+
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+
+game_is_on=True
+
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
