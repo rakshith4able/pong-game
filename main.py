0bdac1e6e3683b6387f0548ab05e470783900811
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2021-11-08 14:28:31 +0530

    Created Ball and made it to  move

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 
@@ -7,8 +9,9 @@ screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
-l_paddle = Paddle((-350,0))
-r_paddle =  Paddle((350,0))
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
+ball = Ball()
 screen.update()
 screen.listen()
 
@@ -17,10 +20,12 @@ screen.onkey(l_paddle.go_down, "s")
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
 
-
-game_is_on=True
+game_is_on = True
 
 while game_is_on:
+    time.sleep(.1)
     screen.update()
+    ball.move()
+
 
 screen.exitonclick()
